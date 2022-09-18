be2c3b4c0346373fd4d2a0593bca615b804ee2cd
Atlas Kaplan <kxplxn@icloud.com>
2022-09-18 14:46:11 +0100

    Linked list implementation is added.

---
+++ data-structures/01_linked_list.go
@@ -0,0 +1,44 @@
+package main
+
+// https://www.geeksforgeeks.org/what-is-linked-list/
+
+type List struct {
+	head *Node
+	tail *Node
+}
+
+func (l *List) First() *Node {
+	return l.head
+}
+
+func (l *List) Push(value int) {
+	node := &Node{value: value}
+	if l.head == nil {
+		l.head = node
+	} else {
+		l.tail.next = node
+	}
+	l.tail = node
+}
+
+type Node struct {
+	value int
+	next  *Node
+}
+
+func (n *Node) Next() *Node {
+	return n.next
+}
+
+func main() {
+	l := &List{}
+	l.Push(1)
+	l.Push(2)
+	l.Push(3)
+
+	n := l.First()
+	for n != nil {
+		println(n.value)
+		n = n.Next()
+	}
+}
