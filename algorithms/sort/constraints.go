5450e9426c673a556020f560d0a5dc11971cb4a5
Atlas Kaplan <kxplxn@icloud.com>
2022-09-21 09:15:10 +0100

    Quick sort implementation is added.

--- algorithms/sort/constraints.go
+++ algorithms/sort/constraints.go
@@ -1,10 +1,5 @@
 package sort
 
-// Package constraints has some useful generic type constraints defined which is very similar to
-// [golang.org/x/exp/constraints](https://pkg.go.dev/golang.org/x/exp/constraints) package.
-// We refrained from using that until it gets placed into the standard library - currently
-// there are some questions regarding this package [ref](https://github.com/golang/go/issues/50792).
-
 // Signed is a generic type constraint for all signed integers.
 type Signed interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64
@@ -31,11 +26,6 @@ type Number interface {
 }
 
 // Ordered is a generic type constraint for all ordered data types in Go.
-// Loosely speaking, in mathematics a field is an ordered field if there is a "total
-// order" (a binary relation - in this case `<` symbol) such that we will always have
-// if a < b then a + c < b + c and if 0 < a, 0 < b then 0 < a.b
-// The idea in Go is quite similar, though only limited to Go standard types
-// not user defined types.
 type Ordered interface {
 	Integer | ~string | Float
 }
---
+++ algorithms/sort/quick/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/kxplxn/learn_go-dsadp/algorithms/sort"
+	"log"
+	"math/rand"
+)
+
+func Partition[T sort.Ordered](arr []T, low, high int) int {
+	iSwap := low
+	pivotItem := arr[high]
+
+	for i := low; i < high; i++ {
+		if arr[i] <= pivotItem {
+			arr[iSwap], arr[i] = arr[i], arr[iSwap]
+			iSwap++
+		}
+	}
+
+	arr[iSwap], arr[high] = pivotItem, arr[iSwap]
+	return iSwap
+}
+
+func QuickSortRange[T sort.Ordered](arr []T, low, high int) {
+	if len(arr) < 2 {
+		return
+	}
+
+	if low < high {
+		iPivot := Partition(arr, low, high)
+		QuickSortRange(arr, low, iPivot-1)
+		QuickSortRange(arr, iPivot+1, high)
+	}
+}
+
+func QuickSort[T sort.Ordered](arr []T) {
+	QuickSortRange(arr, 0, len(arr)-1)
+}
+
+func main() {
+	var arr []int
+	for i := 0; i < 10; i++ {
+		arr = append(arr, rand.Intn(101))
+	}
+
+	log.Printf("before quick sort: %v", arr)
+	QuickSort(arr)
+	log.Printf("after quick sort: %v", arr)
+}
