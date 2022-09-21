d66bf31cd91e869efbcbc879f6f82430f0d5605d
Atlas Kaplan <kxplxn@icloud.com>
2022-09-21 08:08:13 +0100

    Merge sort implementation is added.

---
+++ algorithms/sort/merge/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"github.com/kxplxn/learn_go-dsadp/algorithms/sort"
+	"log"
+	"math/rand"
+)
+
+//https://www.geeksforgeeks.org/merge-sort/
+
+func MergeSort[T sort.Ordered](arr []T) []T {
+	if len(arr) < 2 {
+		return arr
+	}
+
+	mid := len(arr) / 2
+	l := MergeSort(arr[:mid])
+	r := MergeSort(arr[mid:])
+	return merge(l, r)
+}
+
+func merge[T sort.Ordered](l, r []T) []T {
+	res := make([]T, len(l)+len(r))
+
+	iL, iR := 0, 0
+	for iL < len(l) && iR < len(r) {
+		if l[iL] <= r[iR] {
+			res[iL+iR] = l[iL]
+			iL++
+		} else {
+			res[iL+iR] = r[iR]
+			iR++
+		}
+	}
+
+	for iL < len(l) {
+		res[iL+iR] = l[iL]
+		iL++
+	}
+
+	for iR < len(r) {
+		res[iL+iR] = r[iR]
+		iR++
+	}
+
+	return res
+}
+
+func main() {
+	var arr []int
+	for i := 0; i < 10; i++ {
+		arr = append(arr, rand.Intn(100))
+	}
+
+	log.Printf("before merge sort: %v", arr)
+	arr = MergeSort(arr)
+	log.Printf("after merge sort: %v", arr)
+}
