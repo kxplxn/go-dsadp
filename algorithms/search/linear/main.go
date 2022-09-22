1962cda839c303778fcb7ea7d84ca3209293c62d
Atlas Kaplan <kxplxn@icloud.com>
2022-09-22 15:08:00 +0100

    Linear search implementation is added.

---
+++ algorithms/search/linear/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"errors"
+	"log"
+)
+
+func LinearSearch(items []string, query string) (int, error) {
+	for i, v := range items {
+		if v == query {
+			return i, nil
+		}
+	}
+	return -1, errors.New("not found")
+}
+
+func main() {
+	items := []string{"box", "red", "joke", "crooked", "delight"}
+
+	query := "crooked"
+	if i, err := LinearSearch(items, query); err != nil {
+		log.Printf("err: %v\n", err)
+	} else {
+		log.Printf("%v found at index %v", query, i)
+	}
+
+	query = "wok"
+	if i, err := LinearSearch(items, query); err != nil {
+		log.Printf("err: %v\n", err)
+	} else {
+		log.Printf("%v found at index %v", query, i)
+	}
+}
