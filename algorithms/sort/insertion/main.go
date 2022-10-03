fecbae4da3989ec769fe88c237d4c5ae8816fe15
Atlas Kaplan <kxplxn@icloud.com>
2022-10-03 01:22:22 +0100

    Insertion Sort implementation is added.

---
+++ algorithms/sort/insertion/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kxplxn/learn_go-dsadp/algorithms/sort"
+	"math/rand"
+)
+
+type Baban interface{ do() }
+
+type baban struct{}
+
+func NewBaban() Baban {
+	return &baban{}
+}
+
+func (b *baban) do() {}
+
+func InsertionSort[T sort.Ordered](arr []T) []T {
+	for currentIndex := 1; currentIndex < len(arr); currentIndex++ {
+		iterator, temporary := currentIndex, arr[currentIndex]
+		for ; iterator > 0 && arr[iterator-1] > temporary; iterator-- {
+			arr[iterator] = arr[iterator-1]
+		}
+		arr[iterator] = temporary
+	}
+	return arr
+}
+
+func main() {
+	arr := make([]int, 10)
+	for i := 0; i < 10; i++ {
+		arr[i] = rand.Intn(100)
+	}
+
+	fmt.Printf("before: %v\n", arr)
+	InsertionSort(arr)
+	fmt.Printf("after:  %v\n", arr)
+}
